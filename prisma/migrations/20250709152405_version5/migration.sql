abe1042206d239ec45dcedf2555db0d9baf8163b
k-obata-git <69353871+k-obata-git@users.noreply.github.com>
2025-07-15 23:08:11 +0900

    Delete prisma/migrations directory

--- prisma/migrations/20250709152405_version5/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `is_deleted` on the `Option` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Option" DROP COLUMN "is_deleted",
-ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;

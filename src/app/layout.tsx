d9ed1a2ea2556e0a35bb20545bb79a2f6c079d22
k-obata-git <kenta5512@gmail.com>
2025-07-27 00:59:33 +0900

    Analytics、SpeedInsightsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import { CustomProviders } from "./Providers/providers";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +34,8 @@ export default function RootLayout({
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <CustomProviders>{children}</CustomProviders>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
